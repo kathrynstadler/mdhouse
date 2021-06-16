643d6ff0ce761c00562c06688e1a3d66bd691502
khs6j <66985912+khs6j@users.noreply.github.com>
2021-06-16 18:58:02 -0400

    landing page

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!-- 
+Landing page
+Elements are contained in a grid for box display 
+ -->
+
+ <?php include "templates/header.php"; ?>
+
+<div class="grid-container"> 
+	<div class="item1" style="display: flex; justify-content: flex-end;"> <button class="land" onclick="location.href='create.php'"> Register a Madison House Member </button> 
+	</div> 
+	<div class="item2" style="display: flex; justify-content: flex-start;"> <button class="land" onclick="location.href='read.php'"> Find a Member of Madison House </button>
+	</div>
+	<div class="item3" style="display: flex; justify-content: flex-end;"> <button class="land" onclick="location.href='update.php'"> Update a Madison House Member </button>
+	</div>
+	<div class="item4" style="display: flex; justify-content: flex-start;"> <button class="land" onclick="location.href='delete.php'"> Delete a Madison House Member </button>
+	</div>
+</div> 
+
+<?php include "templates/footer.php"; ?>
